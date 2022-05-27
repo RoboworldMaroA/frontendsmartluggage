f239460541ad99c7ea56a3f569788d84774cca52
RoboworldMaroA <augustynmarek@yahoo.co.uk>
2022-05-27 18:54:26 +0100

    add missing end point

--- src/DisplayStateOfCustomerList.js
+++ src/DisplayStateOfCustomerList.js
@@ -50,7 +50,7 @@ export const DisplayStateOfCustomerList = () => {
     useEffect(() => {
         //do something when loading
         console.log("yoyw yow do something from useeffect")
-        fetch("api/v1/trip/").then((response) => response.json()).then((dataTrip) => {
+        fetch("https://backendsmartluggage.herokuapp.com/api/v1/trip/").then((response) => response.json()).then((dataTrip) => {
             console.log("List of items in the trip", dataTrip);
             setTrip(dataTrip);
         });
