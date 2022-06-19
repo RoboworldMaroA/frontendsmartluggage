3faa8c0aba0e32878b1cea1fea2fc0012136129a
RoboworldMaroA <augustynmarek@yahoo.co.uk>
2022-06-19 13:40:31 +0100

    Login Update 4

--- src/components/LoginNew2.js
+++ src/components/LoginNew2.js
@@ -97,7 +97,7 @@ export const LoginNew2 = ({setUserData: setUser}) => {
 
 
             // fetch("login", {
-            fetch("login", {
+            fetch("https://backendsmartluggage.herokuapp.com/login", {
             method: "POST",
             body: JSON.stringify({userName,password,active,roles,userAddress,userDob,userEmail,userPhoneNumber, userSurname}),
             headers: {
