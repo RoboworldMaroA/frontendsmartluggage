1152b61fca8e9cc71d2284965fb8ec150c875b84
RoboworldMaroA <augustynmarek@yahoo.co.uk>
2022-12-09 15:05:50 +0000

    fix display customer luggage

--- src/DisplayStateOfCustomerList.js
+++ src/DisplayStateOfCustomerList.js
@@ -31,7 +31,7 @@ export const DisplayStateOfCustomerList = () => {
         //do something when loading
         console.log("useEffect")
         if (!customer) {
-            fetch("https://backendsmartluggage.herokuapp.com/api/v1/customer").then((response) => response.json()).then((dataCustomer) => {
+            fetch("https://smartluggagebackend.herokuapp.com/api/v1/customer").then((response) => response.json()).then((dataCustomer) => {
                 // This is format before deploy
                 // fetch("api/v1/customer").then((response) => response.json()).then((dataCustomer) => {
                 console.log("List of items in the customer", dataCustomer);
@@ -86,7 +86,7 @@ export const DisplayStateOfCustomerList = () => {
     useEffect(() => {
         //do something when loading
         console.log("yoyw yow do something from useeffect")
-        fetch("https://backendsmartluggage.herokuapp.com/api/v1/trip").then((response) => response.json()).then((dataTrip) => {
+        fetch("https://smartluggagebackend.herokuapp.com/api/v1/trip").then((response) => response.json()).then((dataTrip) => {
             console.log("List of items in the trip", dataTrip);
             setTrip(dataTrip);
         });
