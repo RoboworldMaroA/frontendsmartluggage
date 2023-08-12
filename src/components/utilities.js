65799f26b9e762174c441ea4fa1babc42a568842
RoboworldMaroA <augustynmarek@yahoo.co.uk>
2023-08-12 11:22:50 +0100

    hidden vision system

--- src/components/utilities.js
+++ src/components/utilities.js
@@ -1,25 +1,25 @@
-export const drawRect = (detections, ctx) => {
-
-
-    detections.forEach(predictions => {
-        //get predictions result
-        const [x,y,width,height] = predictions['bbox'];
-        const text = predictions['class'];
-
-        //set styling
-        const color ='red'
-        ctx.strokeStyle = color
-        ctx.font = '22px Arial'
-        ctx.fillStyle = color
-
-        //draw rectangles and text
-        ctx.beginPath()
-        ctx.fillText(text, x, y)
-        ctx.rect(x, y, width, height)
-        ctx.stroke()
-
-
-    })
-
-
-}
+// export const drawRect = (detections, ctx) => {
+//
+//
+//     detections.forEach(predictions => {
+//         //get predictions result
+//         const [x,y,width,height] = predictions['bbox'];
+//         const text = predictions['class'];
+//
+//         //set styling
+//         const color ='red'
+//         ctx.strokeStyle = color
+//         ctx.font = '22px Arial'
+//         ctx.fillStyle = color
+//
+//         //draw rectangles and text
+//         ctx.beginPath()
+//         ctx.fillText(text, x, y)
+//         ctx.rect(x, y, width, height)
+//         ctx.stroke()
+//
+//
+//     })
+//
+//
+// }
