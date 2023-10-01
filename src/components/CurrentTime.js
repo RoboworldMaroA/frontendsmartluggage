891a2e6b94d6ffe4482bdc7007a71a48d95eb548
RoboworldMaroA <augustynmarek@yahoo.co.uk>
2023-10-01 14:29:45 +0100

    update CurrentTime.js

--- src/components/CurrentTime.js
+++ src/components/CurrentTime.js
@@ -1,11 +1,61 @@
 
 import React, {useEffect, useState} from 'react';
 import '../allPages.css'
+import axios from "axios";
+
+
+
 
 
 
 
 const CurrentTime = () => {
+
+
+
+    //WAKEUP BACKEND ON HEROKU
+    const [customer, setCustomer] = useState(null);
+    const [customer2, setCustomer2] = useState(null);
+    const [trip, setTrip] = useState(null);
+
+    let backEndUrl = "https://smartluggagebackendjwt-c266cf5456e9.herokuapp.com/api/v1/trip";
+
+//new fetch data using axios instead of fetch
+    useEffect(() => {
+        //do something when loading
+        // console.log("test do something from use effect")
+        axios.get(backEndUrl).then((dataTrip) => {
+            console.log("List of items in the trip AXIOS", dataTrip);
+            setTrip(dataTrip.data);
+        });
+    }, ["trip"]);
+
+
+    useEffect(() => {
+        if (!customer) {
+            fetch("https://smartluggagebackend.herokuapp.com/displayUsers").then((response) => response.json()).then((dataCustomer2) => {
+                // console.log("List of items in the customer", dataCustomer);
+                setCustomer2(dataCustomer2);
+            });
+        }
+    }, [customer2]);
+
+
+
+    //New Deploy version
+    useEffect(() => {
+        //do something when loading
+        // console.log("useEffect")
+        if (!customer) {
+            fetch("https://smartluggagebackend.herokuapp.com/api/v1/customer").then((response) => response.json()).then((dataCustomer) => {
+                // This is format before deploy
+                // fetch("api/v1/customer").then((response) => response.json()).then((dataCustomer) => {
+                // console.log("List of items in the customer", dataCustomer);
+                setCustomer(dataCustomer);
+            });
+        }
+    }, [customer]);
+
 
 
 
